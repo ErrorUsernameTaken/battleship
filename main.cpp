c3a347f509e65f5c9d74c1b260efb3bca14a6606
ErrorUsernameTaken <tsteller03@icloud.com>
2018-04-16 16:44:35 -0400

    final?

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 const int rows = 10;
 const int elements = 15;
-int ships = 12;
+int ships = 10;
 int grid[rows][elements];
 
 void makeGrid() {
@@ -39,8 +39,8 @@ int shipNum() {
 void setShips() {
     int s = 0;
     while(s < ships) {
-        int shipX = rand() % 12;
-        int shipY = rand() % 12;
+        int shipX = rand() % 10;
+        int shipY = rand() % 10;
         if(grid[shipX][shipY] != 1) {
             s++;
             grid[shipX][shipY] = 1;
@@ -58,15 +58,31 @@ bool attack(int shipX, int shipY) {
 }
 
 int main() {
+    srand(time(NULL));
     makeGrid();
-    showGrid();
-    /*
-    cout << "---------------------------" << endl;
     setShips();
+    int pos1, pos2;
+    char giveUp;
+    while (1) {
+        cout << "Input the X location of your target. "; cin >> pos1 ;
+        cout << "Input the Y location of your target. "; cin >> pos2;
+        if (attack(pos1, pos2)) {
+            cout << "You sunk a battleship." << endl;
+        } else {
+            cout << "Try again." << endl;
+            cout << "There are " << shipNum() << " ships left." << endl;
+            cout << "Do you wish to give up? (y/n)"; cin >> giveUp;
+            if (giveUp == 'y') {
+                break;
+            }
+        }
+    }
+    cout << "You have chosen to give up." << endl;
+    cout << endl;
     showGrid();
-    attack(1, 1);
-    cout << "There are " << shipNum() << " ships left." << endl;
-    */
+    cout << endl;
+    cout << "0 = Grid Spaces" << endl;
+    cout << "1 = Ships" << endl;
+    cout << "2 = Ships Sunk" << endl;
     return 0;
 }
-
